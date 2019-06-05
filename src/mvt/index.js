35584fba4ca5b3586cc41d401f51baa340fff371
zzcyrus <zzcyrus@outlook.com>
2019-06-05 15:08:32 +0800

    feat: update mvt example

--- src/mvt/index.js
+++ src/mvt/index.js
@@ -4,7 +4,36 @@ const server = serverStore()
 const headers = require('tilestrata-headers')
 
 server
-  .layer('osm_mvt')
+  .layer('osm_mvt_admin')
+  .route('tile.mvt')
+  .use(
+    headers({
+      'Access-Control-Allow-Origin': '*'
+    })
+  )
+  .use(
+    postgismvt({
+      lyr: {
+        table: 'import.osm_admin',
+        geometry: 'geometry',
+        srid: 3857,
+        minZoom: 1,
+        maxZoom: 19,
+        buffer: 10,
+        fields: 'name',
+        resolution: 256
+      },
+      pgConfig: {
+        host: 'localhost',
+        user: 'gis',
+        password: 'gis',
+        database: 'gis',
+        port: '6543'
+      }
+    })
+  )
+server
+  .layer('osm_mvt_buildings')
   .route('tile.mvt')
   .use(
     headers({
@@ -16,9 +45,38 @@ server
       lyr: {
         table: 'import.osm_buildings',
         geometry: 'geometry',
-        type: 'circle',
         srid: 3857,
-        minZoom: 3,
+        minZoom: 1,
+        maxZoom: 19,
+        buffer: 10,
+        fields: 'name',
+        resolution: 256
+      },
+      pgConfig: {
+        host: 'localhost',
+        user: 'gis',
+        password: 'gis',
+        database: 'gis',
+        port: '6543'
+      }
+    })
+  )
+
+server
+  .layer('osm_mvt_amenities')
+  .route('tile.mvt')
+  .use(
+    headers({
+      'Access-Control-Allow-Origin': '*'
+    })
+  )
+  .use(
+    postgismvt({
+      lyr: {
+        table: 'import.osm_amenities',
+        geometry: 'geometry',
+        srid: 3857,
+        minZoom: 1,
         maxZoom: 19,
         buffer: 10,
         fields: 'name',
